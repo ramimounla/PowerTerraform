2df55aa95568f8a170344d57de039daf7783c66f
Rami Mounla <Rami_Mounla@hotmail.com>
2020-03-02 00:31:27 +1300

    ESB and queue example

---
+++ ESB/ESB_Queue.tf
@@ -0,0 +1,46 @@
+
+#############################################################################
+# VARIABLES
+#############################################################################
+provider "azurerm" {
+  version = "~> 2.0.0"
+  features {}
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "service_bus_namespace" {
+  type    = string
+}
+
+variable "queue_name" {
+  type = string
+}
+
+# Data Sources
+data "azurerm_resource_group" "resource_group" {
+  name                = var.resource_group_name
+}
+
+# Resources
+resource "azurerm_servicebus_namespace" "servicebus" {
+  name                = var.service_bus_namespace
+  location            = data.azurerm_resource_group.resource_group.location  
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  sku                 = "Basic"
+
+  tags = {
+    source = "terraform"
+  }
+}
+
+resource "azurerm_servicebus_queue" "queue" {
+  name                = var.queue_name
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+  namespace_name      = azurerm_servicebus_namespace.servicebus.name
+
+  enable_partitioning = false
+  dead_lettering_on_message_expiration = true
+}
