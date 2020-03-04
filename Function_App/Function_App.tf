d830ac1efb1d8247a1701467c996d194ed2b5553
Rami Mounla <Rami_Mounla@hotmail.com>
2020-03-04 20:17:07 +1300

    Azure Function App content

---
+++ Function_App/Function_App.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+  version = "~> 2.0.0"
+  features {}
+}
+
+################################
+# VARIABLES
+################################
+variable "resource_group_name" {
+  type = string
+}
+
+variable "storage_account_name" {
+  type    = string
+}
+
+variable "app_service_plan_name" {
+  type    = string
+}
+
+variable "function_name" {
+  type = string
+}
+
+################################
+# DATA SOURCES
+################################
+data "azurerm_resource_group" "resource_group" {
+  name                = var.resource_group_name
+}
+
+################################
+# RESOURCES
+################################
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = data.azurerm_resource_group.resource_group.name
+  location                 = data.azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = var.app_service_plan_name
+  location            = data.azurerm_resource_group.resource_group.location
+  resource_group_name = data.azurerm_resource_group.resource_group.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "example" {
+  name                      = var.function_name
+  location                  = data.azurerm_resource_group.resource_group.location
+  resource_group_name       = data.azurerm_resource_group.resource_group.name
+  app_service_plan_id       = azurerm_app_service_plan.example.id
+  storage_connection_string = azurerm_storage_account.example.primary_connection_string
+}
