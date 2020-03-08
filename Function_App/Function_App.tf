26c16501350585e2d8c9e20ab1fc30da188a9353
Rami Mounla <Rami_Mounla@hotmail.com>
2020-03-08 22:00:55 +1300

    Added access policy to keyvault

--- Function_App/Function_App.tf
+++ Function_App/Function_App.tf
@@ -10,6 +10,10 @@ variable "resource_group_name" {
   type = string
 }
 
+variable "keyvault" {
+  type = string
+}
+
 variable "storage_account_name" {
   type    = string
 }
@@ -27,6 +31,11 @@ variable "function_name" {
 ################################
 data "azurerm_resource_group" "resource_group" {
   name                = var.resource_group_name
+}
+
+data "azurerm_key_vault" "vault" {
+  name                = var.keyvault
+  resource_group_name = var.resource_group_name
 }
 
 ################################
@@ -57,4 +66,14 @@ resource "azurerm_function_app" "example" {
   resource_group_name       = data.azurerm_resource_group.resource_group.name
   app_service_plan_id       = azurerm_app_service_plan.example.id
   storage_connection_string = azurerm_storage_account.example.primary_connection_string
+}
+
+resource "azurerm_key_vault_access_policy" "item" {
+  key_vault_id    = data.azurerm_key_vault.vault.id
+  tenant_id       = azurerm_function_app.example.identity.0.tenant_id
+  object_id       = azurerm_function_app.example.identity.0.principal_id
+
+  secret_permissions = [
+    "get",
+  ]
 }
