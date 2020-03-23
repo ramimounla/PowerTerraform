9dc0d6ac44cf5775f91c627cc2fb5ce66099fc24
Rami Mounla <Rami_Mounla@hotmail.com>
2020-03-24 00:20:18 +1200

    Added azure provider for resource group

--- ResourceGroup/ResourceGroup.tf
+++ ResourceGroup/ResourceGroup.tf
@@ -1,3 +1,14 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  tenant_id = var.azure_tenant_id
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secrete = var.azure_secret
+  features {}
+}
+
 ################################
 # VARIABLES
 ################################
