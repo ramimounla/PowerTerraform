65c3a6e97cfaa0b7e6106ca044e2babc0acab2ec
Rami Mounla <Rami_Mounla@hotmail.com>
2020-03-29 00:24:09 +1300

    Updated LogicApp to V2.0

--- LogicApps/LogicApp.tf
+++ LogicApps/LogicApp.tf
@@ -1,7 +1,8 @@
 # Configure the Azure Provider
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=1.44.0"
+  version = "=2.0.0"
+  features {}
 }
 
 ################################
